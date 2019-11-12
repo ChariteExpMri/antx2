e79a05f96f0170e8938a3fea7ee5df7a531c5a1e
ChariteExpMri <pstkoch@googlemail.com>
2019-11-13 00:51:30 +0100

    v1

--- mritools/ant/antver.m
+++ mritools/ant/antver.m
@@ -41,7 +41,8 @@
 % set up GITHUB repository #bw https://github.com/ChariteExpMri/antx2/
 % -primary checks macOS, Linux MINT
 % - updated [uhelp]
-% #ra 13 Nov 2019 (00:47:38) [maskgenerator] added online link to compare selected regions
+% #ra 13 Nov 2019 (00:47:38) 
+% [maskgenerator] added region-lection list and online link to compare selected regions
 %
 %
 %
