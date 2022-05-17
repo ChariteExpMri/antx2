b2691f0a8068f5dd0f619805d611511204126302
ChariteExpMri <stefan.koch@charite.de>
2022-05-17 10:28:11 +0200

    v1

--- mritools/basic/regexpi2.m
+++ mritools/basic/regexpi2.m
@@ -1,7 +1,6 @@
 
-
-
+%same as regexpi but jelds indizes of matching cells, instead of empty cells
+% function id=regexpi2(cells, str,varargin)
 function id=regexpi2(cells, str,varargin)
-%same as regexpi but jelds indizes of mathing cells , instead of empty cells ('I'm tired to code this again and again)
 id=regexpi(cells,str,varargin{:});
 id=find(cellfun('isempty',id)==0);
