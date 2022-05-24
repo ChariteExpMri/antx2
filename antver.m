25e10aec87e866bef7d22d9ddd2c171c3280ef01
ChariteExpMri <stefan.koch@charite.de>
2022-05-24 09:30:45 +0200

    v1

--- antver.m
+++ antver.m
@@ -693,16 +693,35 @@ s1=[s1(1:lastline); {' '}];
 it=find(~cellfun(@isempty,regexpi(s1,['#\w+.*(\d\d:\d\d:\d\d)'])));
 it(end+1)=size(s1,1);
 
-% https://stackoverflow.com/questions/11509830/how-to-add-color-to-githubs-readme-md-file
-tb(1,:)={ '#yk'    '![#f03c15](https://via.placeholder.com/15/f03c15/000000?text=+) '  'red' } ;
-tb(2,:)={ '#ok'    '![#c5f015](https://via.placeholder.com/15/c5f015/000000?text=+) '  'green' } ;
-tb(3,:)={ '#ra'    '![#1589F0](https://via.placeholder.com/15/1589F0/000000?text=+) '  'blue' } ;
-tb(4,:)={ '#bw'    '![#FF00FF](https://via.placeholder.com/15/FF00FF/000000?text=+) '  'margenta' } ;
-tb(5,:)={ '#gw -->' '&#8618;'  'green arrow' } ;
-tb(6,:)={ '#ba'    '![#1589F0](https://via.placeholder.com/15/1589F0/000000?text=+) '  'blue' } ;
-tb(7,:)={ '#k '     ' '  'remove tag' } ;
-tb(8,:)={ ' #n '    ' '  'remove tag' } ;
-tb(9,:)={ ' #b '    ' '  'remove tag' } ;
+%% ==============[old dots/symbols ... not working anymore]=================================
+if 0
+    % https://stackoverflow.com/questions/11509830/how-to-add-color-to-githubs-readme-md-file
+    tb(1,:)={ '#yk'    '![#f03c15](https://via.placeholder.com/15/f03c15/000000?text=+) '  'red' } ;
+    tb(2,:)={ '#ok'    '![#c5f015](https://via.placeholder.com/15/c5f015/000000?text=+) '  'green' } ;
+    tb(3,:)={ '#ra'    '![#1589F0](https://via.placeholder.com/15/1589F0/000000?text=+) '  'blue' } ;
+    tb(4,:)={ '#bw'    '![#FF00FF](https://via.placeholder.com/15/FF00FF/000000?text=+) '  'margenta' } ;
+    tb(5,:)={ '#gw -->' '&#8618;'  'green arrow' } ;
+    tb(6,:)={ '#ba'    '![#1589F0](https://via.placeholder.com/15/1589F0/000000?text=+) '  'blue' } ;
+    tb(7,:)={ '#k '     ' '  'remove tag' } ;
+    tb(8,:)={ ' #n '    ' '  'remove tag' } ;
+    tb(9,:)={ ' #b '    ' '  'remove tag' } ;
+end
+%% ============[new dots/symbols]===================================
+tb={};
+tb(end+1,:)={ '#ba'    '&#9830; '  'blue' } ;
+
+tb(end+1,:)={ '#yk'    '![#f03c15](https://via.placeholder.com/15/f03c15/000000?text=+) '  'red' } ;
+tb(end+1,:)={ '#ok'    '![#c5f015](https://via.placeholder.com/15/c5f015/000000?text=+) '  'green' } ;
+tb(end+1,:)={ '#ra'    '![#1589F0](https://via.placeholder.com/15/1589F0/000000?text=+) '  'blue' } ;
+tb(end+1,:)={ '#bw'    '![#FF00FF](https://via.placeholder.com/15/FF00FF/000000?text=+) '  'margenta' } ;
+tb(end+1,:)={ '#gw -->' '&#8618;'  'green arrow' } ;
+tb(end+1,:)={ '#k '     ' '  'remove tag' } ;
+tb(end+1,:)={ ' #n '    ' '  'remove tag' } ;
+tb(end+1,:)={ ' #b '    ' '  'remove tag' } ;
+
+
+%% ===============================================
+
 
 
 s2=[];
