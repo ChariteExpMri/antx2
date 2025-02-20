36e52de94fad463b40a089d0b13965192bb04aeb
ChariteExpMri <stefan.koch@charite.de>
2025-02-20 16:34:58 +0100

    v1

--- mritools/various/rmricron.m
+++ mritools/various/rmricron.m
@@ -28,14 +28,15 @@ function  rmricron(pa,tmp,ovl,cols, trans)
 %% EXAMPLE-4
 % rmricron([],fullfile(pwd,'AVGT.nii'))
 %% MAC
-%  MAC uses mricroGL 
+%  MAC uses mricroGL
 % 4th arg is color defined as string or number see mricroGL
 %  example:
 % f1 ='/Volumes/M/data4/macTest/dat/mo75/t2.nii'
 % f2 ='/Volumes/M/data4/macTest/dat/mo75/c1t2.nii'
 % rmricron([],f1,f2,5)
 % rmricron([],f1,f2,'1hot')
-
+% rmricron([],fullfile(pwd,'AVGT.nii'),fullfile(pwd,'AVGTmask.nii'),'actc',.5)
+% rmricron([], 'AVGT.nii','AVGTmask.nii','actc')
 
 if exist('ovl'  )~=1; ovl  =''; end
 if exist('col'  )~=1; col  =''; end
@@ -86,40 +87,75 @@ end
 
 
 tmp=char(fullpath(pa,tmp));
-
+% ======== MAC ============================
 if ismac
-    if exist('cols')==0; cols=1; end
-   if isnumeric(cols)
-       lutname=lutcol(cols);
-   else
-       lutname=cols;
-   end
+    pa_mricrongl=fullfile((fileparts(fileparts(fileparts(which('ant.m'))))),'mricron','MRIcroGL.app/Contents/MacOS/MRIcroGL');
+    k=dir(fullfile(fileparts(fileparts(pa_mricrongl)),'Resources','lut' ,'*.clut'));
+    lutlist={k(:).name}' ;
+    lutlist(regexpi2(lutlist,'^._'))=[];
+    lutlist=regexprep(lutlist,'.clut','');
+    iactc=find(strcmp(lutlist,'actc'));
+    ioth=13;
+    nameoth=lutlist(ioth);
+    lutlist{iactc}=nameoth;
+    lutlist{ioth}='actc';
     
+    if exist('cols')==0; cols=1; end
+    
+    if isnumeric(cols)
+        if cols==0; 
+            lutname='actc';
+        else
+        lutname=char(lutlist(cols));
+        end
+    else
+        lutname=cols;
+    end
+    
+%     lutname='actc';
+    if ~isempty(trans)
+       if trans<=1
+           trans=trans*100;
+       end 
+    else
+        trans=50;
+    end
     
     
     if isempty(ovl)
+        %f1=tmp;
+        %vm=[pa_mricrongl ' -S "begin LOADIMAGE(''' f1 ''');ORTHOVIEW(0.5,0.5,0.5);COLORNAME(''Grayscale'');end."'];
+        %system([vm ' &']);
         f1=tmp;
-        
-        
-        pa_mricrongl=fullfile((fileparts(fileparts(fileparts(which('ant.m'))))),'mricron','MRIcroGL.app/Contents/MacOS/MRIcroGL');
-        vm=[pa_mricrongl ' -S "begin LOADIMAGE(''' f1 ''');ORTHOVIEW(0.5,0.5,0.5);COLORNAME(''Grayscale'');end."'];
-        system([vm ' &']);
-        
+        cm={['import gl']
+            ['gl.loadimage("'   f1 '")']
+            };
+        cm2=strjoin(cm,';');
+        cmd=([pa_mricrongl ' -s ''' cm2 ''' &']);
+        system(cmd);       
     else
         f1=tmp;
         f2=ovl;
-        
-      
-        pa_mricrongl=fullfile((fileparts(fileparts(fileparts(which('ant.m'))))),'mricron','MRIcroGL.app/Contents/MacOS/MRIcroGL');
-        % pa='/Volumes/O/antx/mricron/MRIcroGL.app/Contents/MacOS/MRIcroGL'
-        % f1='/Users/skoch/Desktop/test/x_t2.nii'
-        % f2='/Users/skoch/Desktop/test/ANOpcol.nii'
-        vm=[pa_mricrongl ' -S "begin LOADIMAGE(''' f1 ''');OVERLAYLOAD(''' f2 ''');ORTHOVIEW(0.5,0.5,0.5); OVERLAYCOLORNAME(1,''' lutname ''');end."'];
-        system([vm ' &']);
+        cm={['import gl']
+            %['gl.resetdefaults()']
+            ['gl.loadimage("'   f1 '")']
+            ['gl.overlayload("' f2 '")']
+            %['gl.colorname(2, "actc")' ];
+            ['gl.colorname (0,"grayscale")']
+            ['gl.colorname (1,"' lutname '")']
+            ['gl.opacity(1,' num2str(trans) ')']
+            };
+        cm2=strjoin(cm,';');
+        cmd=([pa_mricrongl ' -s ''' cm2 ''' &']);
+        system(cmd);
     end
     
     return
 end
+
+% ==============================================
+%%   win
+% ===============================================
 
 
 % trans=[20 -1]
@@ -130,9 +166,12 @@ end
 %     'y'  '5'
 %     'c' '6'
 %     }
+if exist('cols')==0
+    cols=0;
+end
 
-if isempty(ovl)
-    o='';
+if isempty(ovl);  
+    o=''; ;
 else
     ov=fullpath(pa,ovl);
     if ischar(ov)
@@ -141,9 +180,13 @@ else
     
     
     % cols=[ 2 2 1 1  ]% 2 2]
-    cols2=[];
-    for i=1:length(ov)
-        cols2{i,1}=[' -c -' num2str(cols(i)) ' '];
+    if ischar(cols)
+        cols2={[' -c ' cols '.lut']};
+    else
+        cols2=[];
+        for i=1:length(ov)
+            cols2{i,1}=[' -c -' num2str(cols(i)) ' '];
+        end
     end
     ov2=cellfun(@(a,b) [' -o ' a b] ,ov,cols2,'UniformOutput',0);
     o = reshape(char(ov2)',   1,  []);
@@ -159,9 +202,9 @@ else
 end
 
 if ispc
-%     call='!start ';
-%     endtag='';
-
+    %     call='!start ';
+    %     endtag='';
+    
     call='';
     endtag=' &';
 else
@@ -178,7 +221,7 @@ if ispc
         call=[call  exefile  ' '];
     end
     [status]= system([call   tmp   o  btrans ttrans endtag]);
-     return
+    return
     
     
 end
@@ -265,11 +308,10 @@ col={ ...
     'revactc'
     'surface'
     'x_rain' };
-    
-    
+
+
 lutname=col{num+1};
-    
-    
-    
-    
-    
+
+
+
+
